927f1776f6dfa7d57b108eadde962078af5b2de9
Fuad <fuadul.hasan@gmail.com>
2019-07-19 00:00:21 -0700

    Update SearchScreen.js

--- App/components/SearchScreen.js
+++ App/components/SearchScreen.js
@@ -4,24 +4,53 @@ import { SafeAreaView, StyleSheet, View, Text, Platform, Button, StatusBar, Touc
 import RNGooglePlaces from 'react-native-google-places';
 import PlaceScreen from './PlaceScreen';
 
+import {G_API} from 'react-native-dotenv';
+
+
 export default class SearchScreen extends Component {
 
+
     openSearchModal() {
+        
         
         RNGooglePlaces.openAutocompleteModal()
         .then((place) => {
             console.log(place);
-            this.props.navigation.navigate("Place",{
-                placeID: place.placeID,
-                rating: place.rating,
-                address: place.address,
-                district: place.addressComponents[2].name
-            });
+            let placeDetails = fetch(
+                `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.placeID}&key=${G_API}`
+            ).then(response => response.json())
+             .then((data) => data.result.photos[0])
+             .then((arr) => {
+                let uri = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=${arr.photo_reference}&key=${G_API}`;
+                fetch(uri)
+                    .then(result => {
+                        this.props.navigation.navigate("Place",{
+                            placeID: place.placeID,
+                            placeName: place.name,
+                            rating: Number(place.rating.toPrecision(2)),
+                            address: place.address,
+                            district: place.addressComponents[2].name,
+                            placeUrl: result.url,
+                            staticLoc:  {
+                                latitude: place.location.latitude.toString(),
+                                longitude: place.location.longitude.toString(),
+                                zoom: 13,
+                                format: 'jpg',
+                                size: {
+                                    width: 300,
+                                    height: 150
+                                },
+                                apiKey: G_API
+                            }
+                        });
+                    })
+             })
+            
         })
         .catch(error => {
             console.log(error.message);
             this.props.navigation.navigate("Home");
-        });  // error is a Javascript Error object
+        });
     }
 
     render(){
